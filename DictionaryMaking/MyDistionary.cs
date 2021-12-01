2c054c9324163efd716b61d1ae7cb5b337a48fcb
Cahit Arslan <chttarslnn@gmail.com>
2021-12-01 04:17:16 +0300

    Add project files.

---
+++ DictionaryMaking/MyDistionary.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+
+namespace DictionaryMaking
+{
+    public class MyDistionary<T1, T2> : IEnumerable where T2 : class
+    {
+        List<T1> list1 = new();
+        List<T2> list2 = new();
+        public MyDistionary()
+        {
+        }
+
+        public MyDistionary(T1 key)
+        {
+            list1.Add(key);
+            list2.Add(null);
+        }
+
+        public MyDistionary(T1 key, T2 value)
+        {
+            list1.Add(key);
+            list2.Add(value);
+        }
+
+        public MyDistionary(MyDistionary<T1,T2> myDistionary)
+        {
+            list1 = myDistionary.list1;
+            list2 = myDistionary.list2;
+        }
+
+        public int Count
+        {
+            get { return list1.Count; }
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            list1.Add(key);
+            list2.Add(value);
+        }
+
+        public void Remove(T1 key)
+        {
+            int counter = 0;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1[i].Equals(key))
+                {
+                    break;
+                }
+                counter++;
+            }
+            list1.RemoveAt(counter);
+            list2.RemoveAt(counter);
+        }
+
+        public void RemoveAt(int index)
+        {
+            list1.RemoveAt(index);
+            list2.RemoveAt(index);
+        }
+
+        public void Clear()
+        {
+            list1.Clear();
+            list2.Clear();
+        }
+
+        public void ShowItems()
+        {
+            Console.WriteLine("\n---Dictionary'de bulunan elemanlar---");
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. eleman çifti :  { list1[i]} - {list2[i]}");
+            }
+            if (list1.Count == 0)
+            {
+                Console.WriteLine("Bu dictionary'de eleman yoktur");
+            }
+        }
+
+
+        //MyDictionary yapısının foreach döngüsüyle itere edilebilmesini sağlar
+        public IEnumerator GetEnumerator()
+        {
+            //return list1.GetEnumerator();
+            for (int i = 0; i < list1.Count; i++)
+            {
+                yield return $"{list1[i]}-{list2[i]}";
+            }
+        }
+    }
+}
---
+++ DictionaryMaking/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace DictionaryMaking // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //parametresiz default constructor
+            MyDistionary<int,string> myDistionary = new MyDistionary<int,string>(); 
+
+            myDistionary.Add(01, "Adana");  //dictionary yapısına ekleme işlemi yapar
+            myDistionary.Add(06, "Ankara");
+            myDistionary.Add(07, "Antalya");
+            myDistionary.Add(34, "İstanbul");
+            myDistionary.Add(35, "İzmir");
+
+            //IEnumerable interface'nin implemente edilmesi sayesinde foreach döngüsünde kullanılabilir
+            foreach (var item in myDistionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            MyDistionary<int,string> myDistionary2 = new MyDistionary<int, string>();
+            myDistionary2 = myDistionary;
+            myDistionary2.ShowItems();  //dictionary'de bulunan değer çiftlerini ekrana yazdırır
+
+            myDistionary2.RemoveAt(0);  //index numarasına göre silme işlemi yapar
+            myDistionary.ShowItems();
+
+            myDistionary2.Remove(07);   //key değerine göre eleman çifti siler
+            myDistionary2.ShowItems();
+
+            myDistionary2.Clear();      //tüm değer çiftlerini siler
+            myDistionary2.ShowItems(); 
+
+            //nesne ayağa kalkarken eleman ekleme, parametreli constructor (string,string)
+            MyDistionary<string,string> myDistionary3 = new MyDistionary<string, string>("book","kitap");
+            myDistionary3.Add("door", "kapı");
+            myDistionary3.Add("pencil", "kalem");
+            myDistionary3.Add("school", "okul");
+            myDistionary3.ShowItems();
+
+
+            //aynı tipten bir nesnenin referansıyla(key ve value verilerini tutan listelerin referansını ileterek),
+            //parametreli constructor (MyDictionary <string,string>)
+            MyDistionary<string, string> myDistionary4 = new MyDistionary<string, string>(myDistionary3);
+            myDistionary4.ShowItems();
+
+
+            Console.ReadLine();
+        }
+    }
+}
